78b61643f318a285f72eefb325b12201927f9616
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-02-12 20:48:40 +0300

    add vytrack dashboard  test

--- src/test/java/com/cydeo/runner/FailedTestRunner.java
+++ src/test/java/com/cydeo/runner/FailedTestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.*;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        glue = "cydeo/step_definitions",
+        glue = "com/cydeo/step_definitions",
         features = "@target/rerun.txt"
 )
 public class FailedTestRunner {
